def5a1137ac0a033a64ec45ebb5b2be934ea744c
Iikka Terho (FMI) <iikka.terho@fmi.fi>
2024-11-04 13:04:36 +0200

    Add feature to arrange players by point, goals or assists (exercise 17 part 1/2)

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -1,6 +1,11 @@
 from player_reader import PlayerReader
+from enum import Enum
 
 
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 class StatisticsService:
     def __init__(self, reader: PlayerReader):
@@ -21,21 +26,27 @@ class StatisticsService:
 
         return list(players_of_team)
 
-    def top(self, how_many):
+    def top(self, how_many, sort_by=SortBy.POINTS):
         # metodin käyttämä apufufunktio voidaan määritellä näin
         def sort_by_points(player):
             return player.points
+        
+        def sort_by_goals(player):
+            return player.goals
+        
+        def sort_by_assists(player):
+            return player.assists
+        
+        sort_key = {
+            SortBy.POINTS: sort_by_points,
+            SortBy.GOALS: sort_by_goals,
+            SortBy.ASSISTS: sort_by_assists
+        }[sort_by]
 
         sorted_players = sorted(
             self._players,
             reverse=True,
-            key=sort_by_points
+            key=sort_key
         )
 
-        result = []
-        i = 0
-        while i <= how_many:
-            result.append(sorted_players[i])
-            i += 1
-
-        return result
+        return sorted_players[:how_many]
