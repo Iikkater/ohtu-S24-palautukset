947c7bec36c4a701ac68365d2013d6f2f3280170
Iikka Terho <iikkater@gmail.com>
2024-11-11 17:04:09 +0200

    Add more grafical playerlist with Rich library (exercise 13)

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,14 +1,34 @@
 from player_reader import PlayerReader
 from player_stats import PlayerStats
+from rich.console import Console
+from rich.table import Table
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    print("NHL statistics by nationality\n")
+    input_year = input("Select season [2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/]: ")
 
-    for player in players:
-        print(player)
+    while True:
+        input_nationality = input("\nSelect nationality [AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR]: ")
+
+        url = f"https://studies.cs.helsinki.fi/nhlstats/{input_year}/players"
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(input_nationality)
+
+        console = Console()
+        table = Table(title=f"Top scorers of {input_nationality} season {input_year}")
+
+        table.add_column("name", justify="left", style="cyan", no_wrap=True)
+        table.add_column("team", justify="left", style="magenta")
+        table.add_column("goals", justify="right", style="green")
+        table.add_column("assists", justify="right", style="green")
+        table.add_column("points", justify="right", style="green")
+
+        for player in players:
+            table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.points))
+
+        console.print(table)
+        print("")
 
 if __name__ == "__main__":
     main()
