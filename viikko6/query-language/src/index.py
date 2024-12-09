371a1738a177f793bc735e7aa11045306ab3142d
Iikka Terho (FMI) <iikka.terho@fmi.fi>
2024-12-09 16:44:04 +0200

    Add Or inquiry (ex. 3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
@@ -41,6 +41,29 @@ def main():
     filtered_with_all = stats.matches(All())
     print(len(filtered_with_all))
 
+    print()
+
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print()
+
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
 
 if __name__ == "__main__":
     main()
